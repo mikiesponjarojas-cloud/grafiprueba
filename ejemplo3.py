ea6b2a74da53ce1d3222d3f7de494b21330d61b3
Maiky <mikiesponjarojas@gmail.com>
2026-02-12 13:55:32 -0600

    Agrego ejemplo3 con separación y mezcla de canales

---
+++ ejemplo3.py
@@ -0,0 +1,29 @@
+import cv2  as cv 
+import numpy as np
+
+
+img1 = cv.imread("C:\\Users\\mikie\\Desktop\\Universidad\\Fondos4k\\Charmander.jpg")
+x,y,z = img1.shape
+print (x,y,z)
+img2 = np.zeros((x,y), np.uint8)
+b,g,r = cv.split(img1)
+mr = cv.merge([img2,img2,r])
+mg = cv.merge([img2, g, img2])
+mb = cv.merge([b,img2,img2])
+nueva = cv.merge([r,g,b])
+nueva2 = cv.merge([g,b,r])
+nueva3 = cv.merge([b,r,g])
+
+
+cv.imshow('n1',nueva)
+cv.imshow('n2',nueva2)
+cv.imshow('n3',nueva3)
+cv.imshow('b', mb)
+cv.imshow('g', mg)
+cv.imshow('r', mr)
+
+
+
+cv.imshow('img', img1)
+cv.waitKey(0)
+cv.destroyAllwindows() 
